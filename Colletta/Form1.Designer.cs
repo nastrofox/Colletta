9ca42dee7fece3a7aace08d47227e0e8a21f5754
crotti.19315 <crotti.19315@LABI1-6PC28>
2023-03-20 08:56:42 +0100

    aggiunta la modifica che va male

--- Colletta/Form1.Designer.cs
+++ Colletta/Form1.Designer.cs
@@ -44,6 +44,10 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // aggiungi
@@ -115,17 +119,17 @@
             // 
             // columnHeader1
             // 
-            this.columnHeader1.Text = "nome";
+            this.columnHeader1.Text = "id";
             this.columnHeader1.Width = 120;
             // 
             // prezzo
             // 
-            this.prezzo.Text = "prezzo";
+            this.prezzo.Text = "nome";
             this.prezzo.Width = 120;
             // 
             // columnHeader2
             // 
-            this.columnHeader2.Text = "colletta totale";
+            this.columnHeader2.Text = "importo";
             this.columnHeader2.Width = 120;
             // 
             // label3
@@ -133,9 +137,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(19, 297);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.Size = new System.Drawing.Size(124, 13);
             this.label3.TabIndex = 8;
-            this.label3.Text = "totale ";
+            this.label3.Text = "obiettivo da raggiungere:";
             // 
             // label4
             // 
@@ -164,6 +168,7 @@
             this.button2.TabIndex = 11;
             this.button2.Text = "nome";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -183,11 +188,50 @@
             this.label5.TabIndex = 13;
             this.label5.Text = "rinomina";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(149, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(41, 127);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(38, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "obiettivo";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(152, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "imposta";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -227,6 +271,10 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Colletta/Form1.cs
+++ Colletta/Form1.cs
@@ -57,32 +57,59 @@ namespace Colletta
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int selezionato = 0;
-            string key;
-            temp = new Partecipante(textBox1.Text, float.Parse(textBox2.Text));
-            if (listView1.SelectedIndices.Count > 0)
-                selezionato = listView1.SelectedIndices[0];
-            key = listView1.SelectedItems[0].Text;
-            if (textBox1.Text != "" && textBox2.Text != "")
+            if (listView1.SelectedItems.Count == 1) // controlla se un solo elemento è selezionato
             {
-                colletta.Modifica(colletta.Raccolta[key], temp);
-                listView1.Items.Clear();
-                foreach (KeyValuePair<string, Partecipante> kvp in colletta.Raccolta)
+                string key = listView1.SelectedItems[0].Text; // ottiene la chiave dell'elemento selezionato
+                if (textBox1.Text != "" && textBox2.Text != "")
                 {
-                    ListViewItem riga = new ListViewItem(colletta.Raccolta[kvp.Key].ToString().Split(';'));
-                    listView1.Items.Add(riga);
+                    colletta.Raccolta[key] = new Partecipante(textBox1.Text, float.Parse(textBox2.Text)); // aggiorna l'elemento nella raccolta
+                    listView1.SelectedItems[0].SubItems[0].Text = textBox1.Text; // aggiorna il nome dell'elemento nella ListView
+                    listView1.SelectedItems[0].SubItems[1].Text = textBox2.Text; // aggiorna il valore dell'elemento nella ListView
+                    textBox1.Text = "";
+                    textBox2.Text = "";
                 }
-                label4.Text = "Totale:" + colletta.QuotaTotale.ToString();
-                textBox1.Text = "";
-                textBox2.Text = "";
+                else
+                {
+                    throw new Exception("Compilare tutte le textbox.");
+                }
             }
             else
-                throw new Exception("compilare tutte le textbox");
+            {
+                throw new Exception("Selezionare un elemento da modificare nella ListView.");
+            }
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Verifica che la ListView abbia almeno una colonna
+            if (listView1.Columns.Count > 0)
+            {
+                // Ottieni gli elementi della ListView e ordina la lista per nome
+                ListViewItem[] items = new ListViewItem[listView1.Items.Count];
+                listView1.Items.CopyTo(items, 0);
+                Array.Sort(items, delegate (ListViewItem x, ListViewItem y) { return x.SubItems[0].Text.CompareTo(y.SubItems[0].Text); });
+
+                // Sostituisci gli elementi nella ListView con quelli ordinati
+                listView1.BeginUpdate();
+                listView1.Items.Clear();
+                listView1.Items.AddRange(items);
+                listView1.EndUpdate();
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            label6.Text = textBox3.Text;
+            textBox3.Clear();
         }
     }
 }
